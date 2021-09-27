438479f60be4912bc3d76c39f005cc44f42dfbe9
Marcos2k02 <marquinhosmaciel44@gmail.com>
2021-09-27 09:58:09 -0300

    Tipos de variaveis

---
+++ atv1.js
@@ -0,0 +1,30 @@
+//a
+var numero = 10;
+console.log(numero);
+
+//b
+var text = "abcde";
+console.log(text);
+
+//c
+var mentira = false;
+var verdade = true;
+
+console.log("pergunta");
+console.log(false || true);
+
+//d
+var cidadeEstado = ["Paulista", "Pernambuco"];
+console.log(cidadeEstado);
+
+//e
+var lugar = {bairro: "Pau Amarelo", cidade: "Paulista", estado: "Pernambuco"};
+console.log(lugar)
+
+//f
+var und;
+console.log("und" + und);
+
+//g
+var vazio = null;
+console.log("vazio" + vazio);
---
+++ atv2.js
@@ -0,0 +1,11 @@
+//1
+var nome = marcos
+var idade = 19
+
+//2
+let nome = marcos
+let idade = 19
+
+//3
+const peso = 80
+const altura = 1.77
